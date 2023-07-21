451933e40083039d2ee79602ee6829bbe97345ba
hyeinjeong <jhi2002@hanyang.ac.kr>
2023-07-21 17:11:35 +0900

    [fix]url 수정 및 url name 추가

--- server/apps/games/urls.py
+++ server/apps/games/urls.py
@@ -8,14 +8,14 @@ app_name = 'games'
 
 urlpatterns = [
     path('game/attack', views.game_attack, name="game_attack"),
-    path('game/<int:pk>/revenge', views.game_revenge, name="game_attack"),
+    path('game/<int:pk>/revenge', views.game_revenge, name="game_revenge"),
     path('rankings', views.game_rank),
     path('', views.main, name="main"),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('list', views.list),
-    path('detail/result/<int:pk>/', views.game_detail_result),
-    path('detail/progress/<int:pk>/', views.game_detail_progress),
-    path('detail/respond/<int:pk>/', views.game_detail_respond),
+    path('list', views.list, name='game_list'),
+    path('detail/result/<int:pk>/', views.game_detail_result, name ='detail_result'),
+    path('detail/progress/<int:pk>/', views.game_detail_progress, name='detail_progress'),
+    path('detail/respond/<int:pk>/', views.game_detail_respond, name='detail_respond'),
 ]
