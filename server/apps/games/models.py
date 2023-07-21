58a58ace87cd72ac00e6ab78c049ff2f40ace06c
braveseokyung <sk100@korea.ac.kr>
2023-07-21 16:26:43 +0900

    [feat] Alter field my_card on game char->int

--- server/apps/games/models.py
+++ server/apps/games/models.py
@@ -16,11 +16,8 @@ class Game(models.Model):
     my_player=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     player=models.ForeignKey(Player,verbose_name="상대",on_delete=models.CASCADE,related_name="games")
     # random_cards=random.sample(range(1,11),5)
-    CARD_CHOICES=[]
-    state=models.IntegerField(default=0) # 진행중, 패배, 승리
+    state=models.IntegerField(default=0) # 진행중, 결과, 반격
     mode=models.IntegerField(default=0) # 높은 점수가 이기는지/낮은 점수가 이기는지
     result=models.IntegerField(default=0) # 게임에서 얻은 점수
-    my_card=models.CharField(
-        choices=CARD_CHOICES, max_length=5,blank=True
-    )
+    my_card=models.IntegerField(default=0)
     player_card=models.IntegerField(default=0)
